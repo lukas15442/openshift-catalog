ba62f796d53112b25e062b242fd76839df05f2ce
lukas15442 <30999809+lukas15442@users.noreply.github.com>
2018-05-24 10:16:36 +0200

    Update catalog.js

--- catalog.js
+++ catalog.js
@@ -11,7 +11,7 @@ window.OPENSHIFT_CONSTANTS.SERVICE_CATALOG_CATEGORIES.unshift({
         // Required.
         label: "PAD",
         // Optional. If specified, defines a unique icon for this item.
-        icon: "hdalogo.jpg",
+        icon: "https://cdn.rawgit.com/lukas15442/openshift-catalog/357fc7ee/hdalogo.jpg",
         // Required. Items matching any tag will appear in this subcategory.
         tags: [
           "hda",
